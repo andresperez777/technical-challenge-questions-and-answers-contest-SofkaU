9f87936d02e811e6c9bc02ffcf8cf87cd3531b3e
andresperez777 <107196335+andresperez777@users.noreply.github.com>
2022-06-09 10:54:13 -0500

    Add files via upload

---
+++ src/test/java/com/ChallengeSofkaUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.challengeSofkaU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeSofkaUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
